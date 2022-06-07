cec237156c84079f77303f631eab7dea191569f5
Xavier Guerrero <xavierignacioguerrero@gmail.com>
2022-06-07 12:48:44 -0300

    1.1 con alert

---
+++ js/alert.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', () => {
+    Swal.fire({
+        title: 'Nicolas!',
+        text: 'Bienvenido a la sección de Manuales',
+        imageUrl: '../assets/logo_header.png',
+        imageWidth: 240,
+        imageHeight: 74,
+        imageAlt: 'Custom image',
+        showConfirmButton: true,
+        confirmButtonText: "INGRESAR",
+        confirmButtonColor: "#48adff", 
+      })
+});
