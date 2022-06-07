c78d3eb17b24c3897506ab78794322322de715a4
Xavier Guerrero <xavierignacioguerrero@gmail.com>
2022-06-07 13:25:01 -0300

    1.3

--- js/alert.js
+++ js/alert.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     Swal.fire({
         title: 'Nicolas!',
         text: 'Bienvenido a la sección de Manuales',
-        imageUrl: '../../assets/logo_header.png',
+        imageUrl: "./assets/logo_header.png",
         imageWidth: 240,
         imageHeight: 74,
         imageAlt: 'Custom image',
